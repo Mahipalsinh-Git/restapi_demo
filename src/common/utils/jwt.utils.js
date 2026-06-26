6e91a722c4deb7aaaa143cec33760d640a6e6f2d
mahipal <mahipalsinh.thakor@gmail.com>
2026-06-26 18:23:01 +0530

    Refactor JWT utility functions and enhance authentication service. Added functions for generating and verifying access and refresh tokens, along with a hashing mechanism. Updated the registration and login services to utilize the new JWT functions, improving token management and user authentication flow.

--- src/common/utils/jwt.utils.js
+++ src/common/utils/jwt.utils.js
@@ -1,6 +1,5 @@
 import crypto from "crypto";
-
-import {} from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 const generateResetToken = () => {
   const rawToken = crypto.randomBytes(32).toString("hex");
@@ -9,4 +8,35 @@ const generateResetToken = () => {
   return { rawToken, hasedToken };
 };
 
-export { generateResetToken };
+const generateHashToken = (token) => {
+  return crypto.createHash("sha256").update(token).digest("hex");
+};
+
+const generateAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+    expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || "15m",
+  });
+};
+
+const verifyAccessToken = (token) => {
+  return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+};
+
+const generateRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || "2d",
+  });
+};
+
+const verifyRefreshToken = (token) => {
+  return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+};
+
+export {
+  generateHashToken,
+  generateResetToken,
+  generateAccessToken,
+  verifyAccessToken,
+  generateRefreshToken,
+  verifyRefreshToken,
+};
